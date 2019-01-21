a98c9991796256f6ebffef8630fe8af674245b0a
denis96z <zinoviev.denis@yandex.ru>
2019-01-21 13:41:51 +0300

    handle not found exception

---
+++ src/main/kotlin/ru/bmstu/iu7/simplemusic/authservice/controller/ExceptionMapper.kt
@@ -0,0 +1,16 @@
+package ru.bmstu.iu7.simplemusic.authservice.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import ru.bmstu.iu7.simplemusic.authservice.exception.NotFoundException
+
+@RestControllerAdvice
+class ExceptionMapper {
+    @ExceptionHandler(NotFoundException::class)
+    fun handleNotFoundException(exception: NotFoundException): ResponseEntity<Error> {
+        val error = Error(exception.message!!)
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error)
+    }
+}
