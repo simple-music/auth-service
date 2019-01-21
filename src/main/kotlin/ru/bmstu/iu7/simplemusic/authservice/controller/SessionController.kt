4486d4d42593fe5fb0f9aba9bf96a7d7bfba798a
denis96z <zinoviev.denis@yandex.ru>
2019-01-21 14:57:08 +0300

    session service methods implemented

--- src/main/kotlin/ru/bmstu/iu7/simplemusic/authservice/controller/SessionController.kt
+++ src/main/kotlin/ru/bmstu/iu7/simplemusic/authservice/controller/SessionController.kt
@@ -1,22 +1,26 @@
 package ru.bmstu.iu7.simplemusic.authservice.controller
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import ru.bmstu.iu7.simplemusic.authservice.model.AuthCredentials
 import ru.bmstu.iu7.simplemusic.authservice.model.RefreshInfo
 import ru.bmstu.iu7.simplemusic.authservice.model.SessionInfo
+import ru.bmstu.iu7.simplemusic.authservice.service.SessionService
 
 @RestController
 @RequestMapping(value = ["/sessions"])
-class SessionController {
+class SessionController(@Autowired private val sessionService: SessionService) {
     @PostMapping
     fun startSession(@RequestBody authCredentials: AuthCredentials): ResponseEntity<SessionInfo> {
-        TODO()
+        val info = this.sessionService.startSession(authCredentials)
+        return ResponseEntity.ok(info)
     }
 
     @PatchMapping
     fun refreshSession(@RequestBody refreshInfo: RefreshInfo): ResponseEntity<SessionInfo> {
-        TODO()
+        val info = this.sessionService.refreshSession(refreshInfo)
+        return ResponseEntity.ok(info)
     }
 
     @DeleteMapping
--- src/main/kotlin/ru/bmstu/iu7/simplemusic/authservice/service/SessionService.kt
+++ src/main/kotlin/ru/bmstu/iu7/simplemusic/authservice/service/SessionService.kt
@@ -1,12 +1,15 @@
 package ru.bmstu.iu7.simplemusic.authservice.service
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import ru.bmstu.iu7.simplemusic.authservice.model.AuthCredentials
 import ru.bmstu.iu7.simplemusic.authservice.model.RefreshInfo
 import ru.bmstu.iu7.simplemusic.authservice.model.SessionInfo
-import ru.bmstu.iu7.simplemusic.authservice.repository.CredentialsRepository
 import ru.bmstu.iu7.simplemusic.authservice.repository.SessionRepository
+import kotlin.random.Random
 
 interface SessionService {
     fun startSession(authCredentials: AuthCredentials): SessionInfo
@@ -15,17 +18,46 @@ interface SessionService {
 }
 
 @Service
-class SessionServiceImpl(@Autowired private val sessionRepository: SessionRepository,
+class SessionServiceImpl(@Value(value = "\${security.token-signing-key}")
+                         private val tokenSigningKey: String,
+                         @Autowired private val sessionRepository: SessionRepository,
                          @Autowired private val credentialsService: CredentialsService) : SessionService {
+    private val algorithm = Algorithm.HMAC256(this.tokenSigningKey)
+
     override fun startSession(authCredentials: AuthCredentials): SessionInfo {
-        TODO("not implemented")
+        val userId = this.credentialsService.getUserId(authCredentials)
+        val sessionInfo = SessionInfo(
+                userId = userId,
+                authToken = this.createAuthToken(userId),
+                refreshToken = this.createRefreshToken(userId)
+        )
+        this.sessionRepository.saveSession(sessionInfo.refreshToken, userId)
+        return sessionInfo
     }
 
     override fun refreshSession(refreshInfo: RefreshInfo): SessionInfo {
-        TODO("not implemented")
+        val userId = this.sessionRepository
+                .getUserId(refreshInfo.refreshToken)
+        return SessionInfo(
+                userId = userId,
+                authToken = this.createAuthToken(userId),
+                refreshToken = refreshInfo.refreshToken
+        )
     }
 
     override fun endSession(refreshInfo: RefreshInfo) {
         TODO("not implemented")
+    }
+
+    private fun createAuthToken(userId: String): String {
+        return JWT.create()
+                .withClaim("userId", userId)
+                .sign(algorithm)
+    }
+
+    private fun createRefreshToken(userId: String): String {
+        return JWT.create()
+                .withClaim("tokenId", "$userId${Random.nextDouble()}")
+                .sign(algorithm)
     }
 }
