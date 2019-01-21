3c4942d10b971b2478c2647a93911fd884b5e917
denis96z <zinoviev.denis@yandex.ru>
2019-01-21 15:12:02 +0300

    timestamp added to auth token

--- src/main/kotlin/ru/bmstu/iu7/simplemusic/authservice/service/SessionService.kt
+++ src/main/kotlin/ru/bmstu/iu7/simplemusic/authservice/service/SessionService.kt
@@ -9,7 +9,9 @@ import ru.bmstu.iu7.simplemusic.authservice.model.AuthCredentials
 import ru.bmstu.iu7.simplemusic.authservice.model.RefreshInfo
 import ru.bmstu.iu7.simplemusic.authservice.model.SessionInfo
 import ru.bmstu.iu7.simplemusic.authservice.repository.SessionRepository
-import kotlin.random.Random
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 interface SessionService {
     fun startSession(authCredentials: AuthCredentials): SessionInfo
@@ -20,9 +22,15 @@ interface SessionService {
 @Service
 class SessionServiceImpl(@Value(value = "\${security.token-signing-key}")
                          private val tokenSigningKey: String,
+                         @Value(value = "\${security.token-lifetime}")
+                         private val tokenLifetime: Int,
                          @Autowired private val sessionRepository: SessionRepository,
                          @Autowired private val credentialsService: CredentialsService) : SessionService {
     private val algorithm = Algorithm.HMAC256(this.tokenSigningKey)
+
+    private val dateTimeFormatter = DateTimeFormatter
+            .ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+            .withZone(ZoneId.of("UTC"))
 
     override fun startSession(authCredentials: AuthCredentials): SessionInfo {
         val userId = this.credentialsService.getUserId(authCredentials)
@@ -50,14 +58,28 @@ class SessionServiceImpl(@Value(value = "\${security.token-signing-key}")
     }
 
     private fun createAuthToken(userId: String): String {
+        val expTime = this.countTokenExpTime()
         return JWT.create()
                 .withClaim("userId", userId)
+                .withClaim("expTime",
+                        this.dateTimeFormatter
+                                .format(expTime.toInstant())
+                )
                 .sign(algorithm)
     }
 
     private fun createRefreshToken(userId: String): String {
         return JWT.create()
-                .withClaim("tokenId", "$userId${Random.nextDouble()}")
+                .withClaim("userId", userId)
                 .sign(algorithm)
+    }
+
+    private fun countTokenExpTime(): Date {
+        val date = Calendar.getInstance()
+        return Date(date.timeInMillis + this.tokenLifetime * TIME_MINUTE)
+    }
+
+    private companion object {
+        private const val TIME_MINUTE = 60000
     }
 }
