69a7840fa915670900b39d23e3c6994869ca3a29
denis96z <zinoviev.denis@yandex.ru>
2019-01-21 13:18:19 +0300

    credentials service implemented

--- src/main/kotlin/ru/bmstu/iu7/simplemusic/authservice/repository/CredentialsRepository.kt
+++ src/main/kotlin/ru/bmstu/iu7/simplemusic/authservice/repository/CredentialsRepository.kt
@@ -2,5 +2,8 @@ package ru.bmstu.iu7.simplemusic.authservice.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import ru.bmstu.iu7.simplemusic.authservice.domain.Credentials
+import java.util.*
 
-interface CredentialsRepository : JpaRepository<Credentials, String>
+interface CredentialsRepository : JpaRepository<Credentials, String> {
+    fun findByUsernameAndPassword(username: String, password: String): Optional<Credentials>
+}
---
+++ src/main/kotlin/ru/bmstu/iu7/simplemusic/authservice/service/CredentialsService.kt
@@ -0,0 +1,65 @@
+package ru.bmstu.iu7.simplemusic.authservice.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import ru.bmstu.iu7.simplemusic.authservice.domain.Credentials
+import ru.bmstu.iu7.simplemusic.authservice.exception.NotFoundException
+import ru.bmstu.iu7.simplemusic.authservice.model.AuthCredentials
+import ru.bmstu.iu7.simplemusic.authservice.model.NewCredentials
+import ru.bmstu.iu7.simplemusic.authservice.repository.CredentialsRepository
+import java.security.MessageDigest
+
+interface CredentialsService {
+    fun addCredentials(newCredentials: NewCredentials)
+    fun getUserId(authCredentials: AuthCredentials): String
+    fun deleteCredentials(userId: String)
+}
+
+@Service
+class CredentialsServiceImpl(
+        @Autowired
+        private val credentialsRepository: CredentialsRepository) : CredentialsService {
+    override fun addCredentials(newCredentials: NewCredentials) {
+        this.credentialsRepository.save(Credentials(
+                userId = newCredentials.userId,
+                username = newCredentials.username,
+                password = this.encodePassword(newCredentials.password)
+        ))
+    }
+
+    override fun getUserId(authCredentials: AuthCredentials): String {
+        val credentials = this.credentialsRepository
+                .findByUsernameAndPassword(
+                        username = authCredentials.username,
+                        password = this.encodePassword(authCredentials.password)
+                )
+        return if (!credentials.isPresent) {
+            throw NotFoundException("credentials not found")
+        } else {
+            credentials.get().userId
+        }
+    }
+
+    override fun deleteCredentials(userId: String) {
+        this.credentialsRepository.deleteById(userId)
+    }
+
+    private fun encodePassword(password: String): String {
+        val bytes = MessageDigest
+                .getInstance("SHA-256")
+                .digest(password.toByteArray())
+        val result = StringBuilder(bytes.size * 2)
+
+        bytes.forEach {
+            val i = it.toInt()
+            result.append(HEX_CHARS[i shr 4 and 0x0f])
+            result.append(HEX_CHARS[i and 0x0f])
+        }
+
+        return result.toString()
+    }
+
+    private companion object {
+        private const val HEX_CHARS = "0123456789ABCDEF"
+    }
+}
