e3f6665e85555b6dd132fa1b17f9da6e036672e6
denis96z <zinoviev.denis@yandex.ru>
2019-01-21 12:55:05 +0300

    session repository added

---
+++ src/main/kotlin/ru/bmstu/iu7/simplemusic/authservice/repository/SessionRepository.kt
@@ -0,0 +1,28 @@
+package ru.bmstu.iu7.simplemusic.authservice.repository
+
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Repository
+import ru.bmstu.iu7.simplemusic.authservice.exception.NotFoundException
+
+interface SessionRepository {
+    fun saveSession(token: String, userId: String)
+    fun getUserId(token: String): String
+    fun deleteSession(token: String)
+}
+
+@Repository
+class SessionRepositoryImpl(@Autowired private val redisTemplate: StringRedisTemplate) : SessionRepository {
+    override fun saveSession(token: String, userId: String) {
+        this.redisTemplate.opsForValue().set(token, userId)
+    }
+
+    override fun getUserId(token: String): String {
+        return redisTemplate.opsForValue().get(token)
+                ?: throw NotFoundException("token not found")
+    }
+
+    override fun deleteSession(token: String) {
+        TODO("not implemented")
+    }
+}
